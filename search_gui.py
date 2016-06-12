28edd5644efa7d4d377f9b4a87672455ecddfce6
Sasi <sasi@gatech.edu>
2016-06-12 04:12:33 -0400

    Separated logic for adding new menu option from search_gui.py
    Moved logic for adding a new menu option in the Anki interface for add-on to the separate file edit_menu.py to make modules more modular

--- search_gui.py
+++ search_gui.py
@@ -79,21 +79,3 @@ class imagesDialog(QDialog):
 
         #     list.addItem(item)
         # outer_layout.addWidget(list)
-
-try:
-    mw.edit_media_submenu.addSeparator()
-except AttributeError:
-    mw.edit_media_submenu = QMenu(u"&Media", mw)
-    mw.form.menuEdit.addSeparator()
-    mw.form.menuEdit.addMenu(mw.edit_media_submenu)
-
-# create a new menu item
-mw.images_search_action = QAction(mw)
-mw.images_search_action.setText(u"Note image")
-# mw.note_download_action.setToolTip(
-#     "Download images for all image fields on this note.")
-
-# set it to call testFunction when it's clicked
-mw.connect(mw.images_search_action, SIGNAL("triggered()"), download_image_for_note)
-
-mw.edit_media_submenu.addAction(mw.images_search_action)
