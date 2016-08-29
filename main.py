b8faf31b5d06cedbefecf464e061f52a20646da7
Sasi <sasi@gatech.edu>
2016-08-29 17:06:06 -0400

    Added local folder to system path

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import json
 
 import aqt.editor
 
+import sys
+print(sys.path)
+sys.path.append(".")
+print(sys.path)
+
+
 # import the main window object (mw) from ankiqt
 from aqt import mw, browser, editor
 # import the "show info" tool from utils.py
@@ -18,9 +24,16 @@ from aqt.editor import Editor
 from anki.hooks import addHook, runHook, wrap
 from anki.consts import MODEL_STD
 
+# import web_search.py
+
+from web_search import review_entries
+from paint_tool import initialize_canvas
+#
+
 #Debugging
 #import code
 # code.interact(local=locals())
+
 
 def setupSearchBrowserButton(self):
     open_button = self._addButton("imagesDialog", lambda self=self: review_entries(),
@@ -29,6 +42,17 @@ def setupSearchBrowserButton(self):
 def setupDrawingCanvasButton(self):
     canvas_button = self._addButton("Canvas", lambda self=self: initialize_canvas(),
             text=u"d\u0336", tip="Canvas (Ctrl+d)", key="Ctrl+d")
+
+
+def gainFocus(note, field):
+    global search_term
+    global currentNote
+    global mediaField
+    # Sets the search term to the field you're on
+    # Note: does not work with newly added fields until restart
+    search_term = note.fields[field]
+    currentNote = note
+    mediaField = field
 
 Editor.setupButtons = wrap(Editor.setupButtons, setupSearchBrowserButton)
 Editor.setupButtons = wrap(Editor.setupButtons, setupDrawingCanvasButton)
